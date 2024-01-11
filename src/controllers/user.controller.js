8616775a55a4675b1706209c7cefd3bc31569a94
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2024-01-11 18:05:01 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -55,7 +55,7 @@ const registerUser = asyncHandler(async (req, res) => {
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path;
-    }
+    } 
 
     //6 files uplaod multer
     if (!avatarLocalPath) {
